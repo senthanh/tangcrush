345e111c9cb71499e552c8d55abbea3e1d81c2aa
Sen Thanh <89731735+senthanh@users.noreply.github.com>
2022-05-16 23:53:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu cậu!',
+    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Cậu yêu tớ có phải không 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'cậu mơ à :333',
+    question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
+    btnReply: 'Gửi cho tớ <3',
+    reply: 'Tại vì cậu đẹp trai hihi <33333333',
+    mess: 'Tớ biết mà 🥰. I love 3000😘😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/114s.0308' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
